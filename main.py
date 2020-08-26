ba220f49de6e292891b7781512489cff980c10e3
emingenc <emingench@gmail.com>
2020-08-26 22:37:41 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+from kivy.uix.screenmanager import Screen
+from screenhelper import *
+
+from kivymd.app import MDApp
+from kivymd.uix.list import OneLineIconListItem
+
+
+Builder.load_string(
+    screen_search
+)
+
+
+class CustomOneLineIconListItem(OneLineIconListItem):
+    icon = StringProperty()
+
+
+class PreviousMDItems(Screen):
+
+    def set_list_md_items(self, text="", search=False):
+
+        def add_item(text):
+            self.ids.rv.data.append(
+                {
+                    "viewclass": "CustomOneLineIconListItem",
+                    "icon": 'language-python',
+                    "text": text,
+                    "callback": lambda x: x,
+                }
+            )
+
+        self.ids.rv.data = []
+        for i in ['a','b','c']:
+            if search:
+                if text in i:
+                    add_item(i)
+            else:
+                add_item(i)
+
+
+class MainApp(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.screen = PreviousMDItems()
+
+    def build(self):
+        return self.screen
+
+    def on_start(self):
+        self.screen.set_list_md_items()
+
+
+MainApp().run()
---
+++ screenhelper.py
@@ -0,0 +1,42 @@
+
+screen_search = '''
+
+
+<CustomOneLineIconListItem>:
+
+    IconLeftWidget:
+        icon: root.icon
+
+
+<PreviousMDItems>:
+
+    BoxLayout:
+        orientation: 'vertical'
+        spacing: dp(10)
+        padding: dp(20)
+
+        BoxLayout:
+            size_hint_y: None
+            height: self.minimum_height
+
+            MDIconButton:
+                icon: 'magnify'
+
+            MDTextField:
+                id: search_field
+                hint_text: 'Search'
+                on_text: root.set_list_md_items(self.text, True)
+
+        RecycleView:
+            id: rv
+            key_viewclass: 'viewclass'
+            key_size: 'height'
+
+            RecycleBoxLayout:
+                padding: dp(10)
+                default_size: None, dp(48)
+                default_size_hint: 1, None
+                size_hint_y: None
+                height: self.minimum_height
+                orientation: 'vertical'
+'''
