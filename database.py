18857996536a2ad40914a5a72818292494abf2f7
emin <emin.genc@e-kalite.com.tr>
2020-08-27 17:25:44 +0300

    optimizations

--- database.py
+++ database.py
@@ -68,8 +68,62 @@ a = "Emin"
 ['string1[::2]','] #adds a third element called step; skips every second character of the string'],
 ["string1[::-1]", "#returns string1's elements in reverse order"]
    ]
-dbnumbers=['numeric']
-dblists=['view-list']
+dbnumbers=['numeric',
+['num1 = 10 \n type(num1)','#checking the type of this variable; integer '],
+['num2 = 2.5  \n type(num2)',' #checking the type of this variable; float  '],
+['int(1.5)','#result is 1 '],
+['float(2)',' #result is 2.0 '],
+['abs(5)','#the distance between the number in between parantheses and 0 '],
+['abs(-5)','#returns the same result as abs(5) '],
+['max(1, 2, 3 ,5, 7)','#returns the largest number '],
+['min(1, 2, 3 ,5, 7)','#returns the smallest number '],
+['pow(3, 2)','#another way of raising to a power '],
+['(1 == 1) and (2 == 2)','#result is True; AND means that both operands should be True in order to get the expression evaluated as True'],
+['(1 == 1) or (2 == 2)', '#result is True; when using OR, it is enough if only one expression is True, in order to have True as the final result '],
+['not(1 == 1)',' #result is False; using the NOT operator means denying an expression, in this case denying a True expression '],
+['not(1 == 2)','#result is True; using the NOT operator means denying an expression, in this case denying a False expression'],
+['None, 0, 0.0, 0L, 0j, empty string, empty list, empty tuple, empty dictionary',' #these values always evaluate to False '],
+['bool(None)','#returns False; function that evaluates values and expressions '],
+['bool(0)',' #returns False; function that evaluates values and expressions '],
+['bool(2)',' #returns True; function that evaluates values and expressions '],
+['bool("router")',' #returns True; function that evaluates values and expressions ']
+
+           ]
+dblists=['view-list',
+['list1 = ["Emin", "New york", "Cow", 3, 10.1, -14]','#creating a list '],
+['len(list)','#returns the number of elements in the list '],
+['list1[0]','#returns "Cisco" which is the first element in the list (index 0) '],
+['list1[0] = "HP"','#replacing the first element in the list with another value '],
+['list2 = [-11, 2, 12]','min(list2)  #returns the smallest element (value) in the list '],
+['list2 = [-11, 2, 12]','max(list2)  #returns the largest element (value) in the list '],
+['list1.append(19)','#appending a new element(19) to the list '],
+['del list1[4]','#removing an element from the list by index'],
+['list1.pop(0)',' #removing an element from the list by index'],
+['list1.remove("HP")','#removing an element from the list by value '],
+['list1.insert(2, "Nortel")','#inserting an element at a particular index '],
+['list1.extend(list2)','#appending a list to another list '],
+['list1.index(-11)',' #returns the index of element -11 '],
+['list1.count(10)','#returns the number of times element 10 is in the list '],
+['list2 = [9, 99, 999, 1, 25, 500]','list2.sort() #sorts the list elements in ascending order; modifies the list in place '],
+['list2.reverse()','#sorts the list elements in descending order; modifies the list in place'],
+['sorted(list2)','#sorts the elements of a list in ascending order and creates a new list at the same time '],
+['sorted(list2, reverse = True)',' #sorts the elements of a list in descending order and creates a new list at the same time '],
+['list1 + list2',' #concatenating two lists '],
+['list1 * 3','#repetition of a list '],
+['a_list[5:15]',' #slice starting at index 5 up to, but NOT including, index 15; so index 14 represents the last element in the slice '],
+['a_list[5:]','#slice starting at index 5 up to the end of the list'],
+['a_list[:10]','#slice starting at the beginning of the list up to, but NOT including, index 10 '],
+['a_list[:]','#returns the entire list '],
+['a_list[-1]','#returns the last element in the list '],
+['a_list[-2]','#returns the second to last element in the list '],
+['a_list[-9:-1]','#extracts a certain sublist using negative indexes '],
+['a_list[-5:]','#returns the last 5 elements in the list '],
+['a_list[:-5]','#returns the list minus its last 5 elements '],
+['a_list[::2]','#adds a third element called step; skips every second element of the list '],
+['a_list[::-1]'," #returns a_list's elements in reverse order "]
+
+         ]
+
 dbsets=['set-none']
 dbtuples=['format-list-text']
 dbranges=['diameter']
