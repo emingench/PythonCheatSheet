bd8a74fa8050c9e63527576ca2313921ae6bddf2
emin <emin.genc@e-kalite.com.tr>
2020-08-28 09:31:57 +0300

    optimizations

--- screenhelper.py
+++ screenhelper.py
@@ -91,54 +91,31 @@ s = '''
                         title: "Python Cheat Sheet"
                         elevation: 11
                         left_action_items: [['menu', lambda x: nav_drawer.toggle_nav_drawer()]]
+                        BoxLayout:
+                            size_hint_y: None
+                            height: self.minimum_height
+                
+                
+                            MDTextField:
+                                id: search_field2
+                                hint_text: 'Search'
+                                color_mode: 'custom'
+                                on_text: root.set_list_md_items(self.text, True)
+                                line_color_focus: 1, 1, 1, 1
+                            
+                            MDIconButton:
+                                icon: 'magnify'
+                                theme_text_color: "Custom"
+                                text_color: 1,1,1,1
                           
                         
                     Widget:
                       
-                    BoxLayout:
-                        size_hint_y: None
-                        height: self.minimum_height
-            
-                        MDIconButton:
-                            icon: 'magnify'
-            
-                        MDTextField:
-                            id: search_field
-                            hint_text: 'Search'
-                            on_text: root.set_list_md_items(self.text, True)  
-                    
 
 
         MDNavigationDrawer:
             id: nav_drawer
 
             ContentNavigationDrawer:
-                BoxLayout:
-                    orientation: 'vertical'
-                    spacing: dp(10)
-                    padding: dp(20)
-                    
-                    BoxLayout:
-                        size_hint_y: None
-                        height: self.minimum_height
-            
-                        MDIconButton:
-                            icon: 'magnify'
-            
-                        MDTextField:
-                            id: search_field2
-                            hint_text: 'Search'
-                            on_text: root.set_list_md_items(self.text, True)
-                    RecycleView:
-                        id: rv2
-                        key_viewclass: 'viewclass'
-                        key_size: 'height'
-
-                    RecycleBoxLayout:
-                        padding: dp(10)
-                        default_size: None, dp(48)
-                        default_size_hint: 1, None
-                        size_hint_y: None
-                        height: self.minimum_height
-                        orientation: 'vertical'
+                Screen:
             '''
